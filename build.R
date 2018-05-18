59419ac04c6258275238aeb7aec88ae4e2482f08
leedrake5 <b.lee.drake@gmail.com>
2018-05-18 12:53:52 -0600

    rPDZ Binary
    Created binary package using RStudio

--- build.R
+++ build.R
@@ -1,7 +1,11 @@
 setwd("/Users/lee/GitHub/rPDZ")
 library(Rcpp)
-Rcpp::Rcpp.package.skeleton("rpdz.cpp")
-setwd("~/GitHub/rPDZ/rpdz.cpp")
+Rcpp::Rcpp.package.skeleton("rPDZ")
+###put .cpp files in
+setwd("/Users/lee/GitHub/rPDZ/rPDZ")
 tools::package_native_routine_registration_skeleton(".", character_only = FALSE)
 Rcpp::compileAttributes(verbose=TRUE)
 
+####in Terminal
+cd "/Users/lee/GitHub/rPDZ"
+sudo R CMD build rPDZ
---
+++ rPDZ/src/rpdz.hpp
@@ -0,0 +1,13 @@
+//
+//  rpdz.hpp
+//  
+//
+//  Created by Lee Drake on 5/16/18.
+//
+
+#ifndef rpdz_hpp
+#define rpdz_hpp
+
+#include <stdio.h>
+
+#endif /* rpdz_hpp */
