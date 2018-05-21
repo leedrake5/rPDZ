94a22a41a3cf814984e5a2e96a5b8a81479cff08
leedrake5 <b.lee.drake@gmail.com>
2018-05-21 14:03:11 -0600

    Works
    Successfully can build for windows now. rPDZ is the master directory. Past tries are numbered.

--- build.R
+++ build.R
@@ -1,11 +1,23 @@
+###in R
 setwd("/Users/lee/GitHub/rPDZ")
 library(Rcpp)
 Rcpp::Rcpp.package.skeleton("rPDZ")
+
+
 ###put .cpp files in
 setwd("/Users/lee/GitHub/rPDZ/rPDZ")
 tools::package_native_routine_registration_skeleton(".", character_only = FALSE)
 Rcpp::compileAttributes(verbose=TRUE)
 
-####in Terminal
+
+####in Terminal - this step creates your R package
 cd "/Users/lee/GitHub/rPDZ"
 sudo R CMD build rPDZ
+
+
+###back in R
+
+###Windows
+devtools::build_win(pkg="/Users/lee/GitHub/rPDZ/rPDZ")
+
+
