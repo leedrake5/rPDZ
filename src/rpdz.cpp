0933e8149cc9311629156adebd8cfc5c1d18e5fc
Lee Drake <b.lee.drake@gmail.com>
2023-10-23 11:37:13 -0600

    Improved file loading
    More (but not perfect) consistency from PDZ 25 files

--- src/rpdz.cpp
+++ src/rpdz.cpp
@@ -9,6 +9,7 @@
 #include <Rcpp.h>
 #include <fstream>
 #include <vector>
+#include <cstdint>
 
 using namespace Rcpp;
 // [[Rcpp::export()]]
@@ -77,55 +78,54 @@ NumericVector readPDZ(std::string fileName, int start, int size) {
 
 // [[Rcpp::export]]
 std::vector<uint32_t> readPDZ25(std::string fileName) {
-    int fileIndex;
-    bool looping = false;
     short recordID;
     int recordLength;
-    uint32_t spectrum[2048];
-    std::vector<char> record(10000000); // This is okay
+    uint32_t spectrum[2048] = {0}; // Initialize to zeros
+    std::vector<char> record(100000000); // This is okay
     
     std::ifstream file(fileName, std::ios::binary);
 
-    if (file.is_open()) {
-        file.read(record.data(), 20); // Change here: use record.data() to get the underlying array
-        fileIndex = 20;
-        while (!looping) {
-            file.read(record.data(), 6); // Change here: use record.data()
-            
-            // Use reinterpret_cast for strict type aliasing, ensuring correct pointer types
-            recordID = *reinterpret_cast<short*>(record.data());
-            recordLength = *reinterpret_cast<int*>(record.data() + 2);
-            
-            if (recordID == 3) {
-                looping = true;
-                file.read(record.data(), recordLength); // Change here: use record.data()
-                int spOff = (*reinterpret_cast<int*>(record.data() + 114)) * 2;
-                spOff += 120;
+    if (!file.is_open()) {
+        Rcpp::stop("Could not open file");
+    }
 
-                for (int i = 0; i < 2048; i++) {
-                    // Access the elements of 'record' correctly
-                    spectrum[i] = *reinterpret_cast<uint32_t*>(record.data() + spOff + i * sizeof(uint32_t));
-                }
-            } else if (recordID == 4) {
-                looping = true;
-                int spOff = 164;
+    file.read(record.data(), 20);
+    if (!file) { Rcpp::stop("Failed to read initial 20 bytes"); }
 
-                for (int i = 0; i < 2048; i++) {
-                    // Access the elements of 'record' correctly
-                    spectrum[i] = *reinterpret_cast<uint32_t*>(record.data() + spOff + i * sizeof(uint32_t));
-                }
-            } else {
-                fileIndex += recordLength + 6;
-                file.seekg(fileIndex);
+    while (true) {  // Using a plain while loop for clarity
+        file.read(record.data(), 6);
+        if (!file) { Rcpp::stop("Failed to read header"); }
+
+        recordID = *reinterpret_cast<short*>(record.data());
+        recordLength = *reinterpret_cast<int*>(record.data() + 2);
+
+        if (recordID == 3 || recordID == 4) {
+            if (recordLength > record.size()) { Rcpp::stop("Record length too large"); }
+
+            file.read(record.data(), recordLength);
+            if (!file) { Rcpp::stop("Failed to read record"); }
+
+            int spOff = (*reinterpret_cast<int*>(record.data() + 114)) * 2;
+            spOff += (recordID == 3) ? 120 : 164;
+
+            if (spOff + 2048 * sizeof(uint32_t) > record.size()) {
+                Rcpp::stop("Spectrum offset out of bounds");
+            }
+
+            for (int i = 0; i < 2048; i++) {
+                spectrum[i] = *reinterpret_cast<uint32_t*>(record.data() + spOff + i * sizeof(uint32_t));
+            }
+            
+            break;  // Exit the loop once you've found and read the spectrum
+        } else {
+            // Seek past the current record to find the next one
+            if (!file.seekg(recordLength, std::ios_base::cur)) {
+                Rcpp::stop("Failed to seek in file");
             }
         }
-        file.close();
-    } else {
-        // Handle the error: the file did not open
-        Rcpp::stop("Could not open file");  // This is how you throw an error in Rcpp
     }
-    
-    // Convert array to vector before returning
+    file.close();
+
     return std::vector<uint32_t>(spectrum, spectrum + 2048);
 }
 
